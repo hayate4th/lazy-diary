1e1e7523d0cf2c06da911e79bda44504086bb380
hayate4th <diabolo.arte3000@gmail.com>
2019-12-20 01:06:16 +0900

    Change test cases for login and sign out buttons

--- src/components/App/index.test.tsx
+++ src/components/App/index.test.tsx
@@ -10,7 +10,7 @@ describe("App", () => {
     expect(loginButton).toBeTruthy();
   });
 
-  it("Login button does not exist after click", () => {
+  it("Login button does not exist after login button is clicked", () => {
     const { getByTestId, queryByTestId } = render(<App />);
     const loginButton = getByTestId(/login-button/i);
 
@@ -19,12 +19,26 @@ describe("App", () => {
     expect(queryByTestId(/login-button/i)).toBeNull();
   });
 
-  it("Logged in text shows after click", () => {
+  it("Sign out button and Logged in text shows after login button is clicked", () => {
     const { getByTestId } = render(<App />);
     const loginButton = getByTestId(/login-button/i);
 
     fireEvent.click(loginButton);
 
+    expect(getByTestId(/sign-out-button/i)).toBeTruthy();
     expect(getByTestId(/logged-in-text/i)).toBeTruthy();
+  });
+
+  // TODO: Find a better way to run this test
+  it("Sign out button and Logged in text does not exist after sign out button is clicked", () => {
+    const { getByTestId, queryByTestId } = render(<App />);
+    const loginButton = getByTestId(/login-button/i);
+    fireEvent.click(loginButton);
+
+    const signOutButton = getByTestId(/sign-out-button/i);
+    fireEvent.click(signOutButton);
+
+    expect(queryByTestId(/sign-out-button/i)).toBeNull();
+    expect(queryByTestId(/logged-in-text/i)).toBeNull();
   });
 });
