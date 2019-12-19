976272efd6ac5ae316937f5f92504ec236ec451e
hayate4th <diabolo.arte3000@gmail.com>
2019-12-19 21:48:10 +0900

    Create configuration and initial files for webpack to move

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  devServer: {
+    contentBase: './public'
+  }
+}
