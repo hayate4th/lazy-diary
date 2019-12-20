ec583dbf2cad278c865914850040c535564e9550
hayate4th <diabolo.arte3000@gmail.com>
2019-12-21 00:01:57 +0900

    Desgin Button component

--- src/components/UserAuthentication/Button/index.tsx
+++ src/components/UserAuthentication/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 interface Props {
   text: string;
@@ -8,10 +9,30 @@ interface Props {
 
 const Button: React.FC<Props> = ({ text, dataTestId, onClickHandler }) => {
   return (
-    <button onClick={onClickHandler} data-testid={dataTestId}>
+    <StyledButton onClick={onClickHandler} data-testid={dataTestId}>
       {text}
-    </button>
+    </StyledButton>
   );
 };
+
+const StyledButton = styled.button`
+  background: none;
+  background-color: #49c6dd;
+  border: none;
+  border-radius: 6px;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 24px;
+  font-weight: bold;
+  padding: 5px 10px;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    background: #c7c4c4;
+  }
+`;
 
 export default Button;
