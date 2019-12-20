d91fe7a5ba20af48af356ef724d0fc8a79df59ab
hayate4th <diabolo.arte3000@gmail.com>
2019-12-20 23:30:56 +0900

    Add dataTestId prop

--- src/components/UserAuthentication/Button/index.tsx
+++ src/components/UserAuthentication/Button/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 
 interface Props {
   text: string;
+  dataTestId: string;
   onClickHandler: () => void;
 }
 
-const Button: React.FC<Props> = ({ text, onClickHandler }) => {
-  return <button onClick={onClickHandler}>{text}</button>;
+const Button: React.FC<Props> = ({ text, dataTestId, onClickHandler }) => {
+  return (
+    <button onClick={onClickHandler} data-testid={dataTestId}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
