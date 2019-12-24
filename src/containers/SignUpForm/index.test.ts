dfabd67f02e4067c5e05e8234049ab2469be7054
hayate4th <diabolo.arte3000@gmail.com>
2019-12-24 14:22:59 +0900

    Add test case for checkIfPasswordsAreSame

--- src/containers/SignUpForm/index.test.ts
+++ src/containers/SignUpForm/index.test.ts
@@ -2,7 +2,7 @@ import { SignUpFormData } from "../../types/SignUpForm";
 import { VisibleForTesting } from ".";
 
 describe("SignUpForm/container", () => {
-  const { checkIfFieldsAreEmpty } = VisibleForTesting;
+  const { checkIfFieldsAreEmpty, checkIfPasswordsAreSame } = VisibleForTesting;
   const baseData: SignUpFormData = {
     email: "",
     password: "",
@@ -29,5 +29,19 @@ describe("SignUpForm/container", () => {
         confirmationPassword: "TEST"
       })
     ).toBeFalsy();
+  });
+
+  it("checkIfPasswordsAreSame returns false when passwords are not the same", () => {
+    expect(checkIfPasswordsAreSame("TEST1", "TEST2")).toBeFalsy();
+  });
+
+  it("checkIfPasswordsAreSame returns false when either passwords are empty strings", () => {
+    expect(checkIfPasswordsAreSame("TEST", "")).toBeFalsy();
+    expect(checkIfPasswordsAreSame("", "TEST")).toBeFalsy();
+    expect(checkIfPasswordsAreSame("", "")).toBeFalsy();
+  });
+
+  it("checkIfPasswordsAreSame returns true when passwords are same", () => {
+    expect(checkIfPasswordsAreSame("TEST", "TEST")).toBeTruthy();
   });
 });
